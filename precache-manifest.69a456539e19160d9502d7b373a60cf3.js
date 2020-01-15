b0cd2a8b9f24d6c1c0704be133bca968cb1452b7
bandvov <bandvov@gmail.com>
2020-01-15 09:17:03 +0200

    first-app

---
+++ precache-manifest.69a456539e19160d9502d7b373a60cf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c511b5f4fb92e0687e7c9a326dc3eb",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "58597898e22389897b93",
+    "url": "/first-app/static/css/main.587c2fb3.chunk.css"
+  },
+  {
+    "revision": "d549a9287c2e51407a5c",
+    "url": "/first-app/static/js/2.010779ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-app/static/js/2.010779ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58597898e22389897b93",
+    "url": "/first-app/static/js/main.c29a7784.chunk.js"
+  },
+  {
+    "revision": "dac035bba098623ba687",
+    "url": "/first-app/static/js/runtime-main.d6ffc792.js"
+  },
+  {
+    "revision": "925bd63ad63217f5d0c98861acbd5d03",
+    "url": "/first-app/static/media/header.925bd63a.jpg"
+  },
+  {
+    "revision": "f7dddfd644e44211321604805b487489",
+    "url": "/first-app/static/media/lenovo-desktop-computer-500x500.f7dddfd6.jpg"
+  }
+]);
